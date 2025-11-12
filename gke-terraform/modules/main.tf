deabf96bdc322c88dd3f15941a7ae68754aff2f6
sowmya12210 <sowmyachukka299@gmail.com>
2025-11-12 22:38:16 +0530

    Lowered node

--- gke-terraform/modules/main.tf
+++ gke-terraform/modules/main.tf
@@ -27,6 +27,8 @@ resource "google_container_node_pool" "primary_nodes" {
   node_config {
     machine_type = var.machine_type
     oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    disk_type     = "pd-ssd"
+    disk_size_gb  = 60
   }
 
   initial_node_count = var.node_count
