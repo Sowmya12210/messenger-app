f65f998cd53410ed3552aad4a9cabb794f39e4b5
sowmya12210 <sowmyachukka299@gmail.com>
2025-11-12 12:47:00 +0530

    Creating bucket

--- gke-terraform/provider.tf
+++ gke-terraform/provider.tf
@@ -1,7 +1,7 @@
 terraform{
     required_providers{
         google = {
-            souce = "hashicorp/google"
+            source = "hashicorp/google"
             version = "~> 6.0"
         }
     }
